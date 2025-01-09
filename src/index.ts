8f5cc193ac13944d082b790999e550d1496d8e3f
Brook Miller <millerbrook@gmail.com>
2025-01-09 16:27:27 -0500

    Basic bot complete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
+import { CronJob } from 'cron';
 
-const text = await Bot.run(getPostText, { dryRun: true });
+const runOnce = async () => {
+	//const text = await Bot.run(getPostText);
+	const text = await Bot.run(getPostText, { dryRun: true} );
 
-console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+	console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+}
+
+const scheduleExpressionMinute = '* * * * *';
+const scheduleExpression: string = '0 */24 * * *';
+const job = new CronJob(scheduleExpression, runOnce);
+job.start();
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,14 @@
+interface Dictionary<T> {
+	[key: string]: T
+}
+
+let data: Dictionary<string> = {
+	post1: 'silly post',
+	post2: 'serious post',
+	post3: 'profound post'
+}
 export default async function getPostText() {
-  // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Hello from the Bluesky API";
+	let post_keys = Object.keys(data);
+	let random_key = post_keys[Math.floor(Math.random()*post_keys.length)];
+  return `Practice bot post: random key ${random_key}; message: ${data[random_key]}`;
 }
